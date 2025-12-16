6db3a20c68893c7b376ae6cfcfaf41023e93893e
KK <38160775+kinkitchen@users.noreply.github.com>
2025-12-16 11:17:58 +0800

    Update ax6_imm_diy-part2.sh

--- sh/ax6_imm_diy-part2.sh
+++ sh/ax6_imm_diy-part2.sh
@@ -1,70 +1,64 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 全局默认仓库和分支
+DEFAULT_REPO="https://github.com/qosmio/openwrt-ipq.git"
+DEFAULT_BRANCH="24.10-nss"
 
 function git_sparse_clone() {
-  branch="$1" rurl="$2" && shift 2
-  rootdir="$PWD"
-  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl temp_sparse
-  #git clone -b $branch --single-branch --no-tags --depth 1 --filter=blob:none --no-checkout $rurl temp_sparse
-  cd temp_sparse
-  git sparse-checkout init --cone
-  git sparse-checkout set $@
-  pkg=`echo $@ | tr ' ' '\n' | rev | cut -d'/' -f 1 | rev | tr '\n' ' ' `
-  #git checkout $branch -- $@
-  [ -d ../package/custom ] && cd ../package/custom && rm -rf $pkg && cd "$rootdir"/temp_sparse
-  mv $@ ../
-  cd ../
-  rm -rf temp_sparse
-  }
-  
+    branch="${1:-$DEFAULT_BRANCH}" 
+    rurl="${2:-$DEFAULT_REPO}" 
+    shift 2
+    rootdir="$PWD"
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$rurl" temp_sparse
+    cd temp_sparse
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    pkg=$(echo "$@" | tr ' ' '\n' | rev | cut -d'/' -f 1 | rev | tr '\n' ' ')
+    [ -d ../package/custom ] && cd ../package/custom && rm -rf $pkg && cd "$rootdir"/temp_sparse
+    mv "$@" ../
+    cd ../
+    rm -rf temp_sparse
+}
+
 function git_svn() {
-  #branch="$1" rurl="$2" localdir="$3" && shift 3
-  branch="$1" rurl="$2" && shift 2
-  rootdir="$PWD"
-  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl temp_svn
-  #git clone -b $branch --single-branch --no-tags --depth 1 --filter=blob:none --no-checkout $rurl temp_svn
-  cd temp_svn
-  git sparse-checkout init --cone
-  git sparse-checkout set $@
-  #pkg=`echo $@ | tr ' ' '\n' | rev | cut -d'/' -f 1 | rev | tr '\n' ' ' `
-  #git checkout $branch -- $@
-  #[ -d ../package/custom ] && cd ../package/custom && rm -rf $pkg && cd "$rootdir"/temp_svn
-  mv $@ ../package/custom/
-  cd ..
-  rm -rf temp_svn
-  }
-  
+    branch="${1:-$DEFAULT_BRANCH}" 
+    rurl="${2:-$DEFAULT_REPO}" 
+    shift 2
+    rootdir="$PWD"
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$rurl" temp_svn
+    cd temp_svn
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    mv "$@" ../package/custom/
+    cd ..
+    rm -rf temp_svn
+}
+
 function merge_package(){
-    branch=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    repo=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $3 | rev | cut -d'/' -f 1 | rev`
-	rootdir="$PWD"
-    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
-    git clone -b $1 --depth=1 --single-branch $2
-    #[ -d package/custom ] && cd package/custom && rm -rf $3 && cd "$rootdir"
-    mv $3 package/custom/
-    rm -rf $repo
+    branch="${1:-$DEFAULT_BRANCH}" 
+    repo="${2:-$DEFAULT_REPO}" 
+    pkg="\$3"
+    rootdir="$PWD"
+    git clone -b "$branch" --depth=1 --single-branch "$repo"
+    mv "$pkg" package/custom/
+    rm -rf $(basename "$repo" .git)
 }
 
 echo "开始 自定义（fichen） 配置……"
 echo "========================="
-#给n2n添加补丁文件，修正前两行语法顺序颠倒的错误
-BASE_PATH=$(cd $(dirname $0)/../ && pwd)
-cp -rf $GITHUB_WORKSPACE/backup/001-fix-cmake-compatibility.patch $BASE_PATH/action_build/feeds/packages/net/n2n/patches/
+BASE_PATH=$(cd $(dirname \$0)/../ && pwd)
 
-##更换luci-theme-design
-#rm -rf $BASE_PATH/action_build/feeds/luci/themes/luci-theme-design $BASE_PATH/action_build/feeds/fichenx/luci-theme-design
-#git_sparse_clone dev https://github.com/fichenx/packages luci-theme-design && cp -af luci-theme-design $BASE_PATH/action_build/feeds/luci/themes/luci-theme-design
-#mv -n luci-theme-design $BASE_PATH/action_build/feeds/fichenx/luci-theme-design
+# 示例：给 n2n 添加补丁
+cp -rf "$GITHUB_WORKSPACE/backup/001-fix-cmake-compatibility.patch" \
+       "$BASE_PATH/action_build/feeds/packages/net/n2n/patches/"
+
+# 示例：拉取自定义 luci-theme-design
+# 使用默认仓库和分支
+git_sparse_clone "$DEFAULT_BRANCH" "$DEFAULT_REPO" luci-theme-design
+cp -af luci-theme-design "$BASE_PATH/action_build/feeds/luci/themes/luci-theme-design"
+
 echo "========================="
 echo " 自定义(fichen) 配置完成……"
